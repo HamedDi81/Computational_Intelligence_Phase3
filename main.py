e8e6eaba9d4ba835f4a2158a1f959332e965119c
Hamed <hamedmasoudi95@gmail.com>
2023-06-14 14:29:23 +0430

    Data Loader

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ import numpy as np
 from typing import Tuple
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+
+# Load the data and label
+def load_data(filename: str) -> Tuple[np.ndarray, np.ndarray]:
+    data = np.load(f'{filename}')
+    return data['data'], data['labels']
+
+# Split the data and labels into training and testing sets
+
+train_data, train_labels = load_data('/content/drive/MyDrive/Copy of train_data_SYN.npz') # Train Data Path
+
+print(train_data.shape)
+print(np.unique(train_labels, return_counts=True))
+
+test_data, test_labels = load_data('/content/drive/MyDrive/Copy of test_data_SYN.npz') # Test Data Path
+
+print(test_data.shape)
+print(np.unique(test_labels, return_counts=True)) 
