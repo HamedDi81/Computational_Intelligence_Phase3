01b0f1468e1f1cb9d64816e4f6169430b11d725b
Hamed <hamedmasoudi95@gmail.com>
2023-06-14 14:24:30 +0430

    py file created

---
+++ main.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+import numpy as np
+from typing import Tuple
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
